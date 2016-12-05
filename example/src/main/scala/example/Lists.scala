819ce74e11849c7ab3ce5014b4fd955b8e13b7df
zgaleday <zgd3200@gmail.com>
2016-12-04 20:51:42 -0500

    Implement add function.

--- example/src/main/scala/example/Lists.scala
+++ example/src/main/scala/example/Lists.scala
@@ -23,7 +23,7 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-    def sum(xs: List[Int]): Int = ???
+    def sum(xs: List[Int]): Int = if (xs.isEmpty) 0 else xs.head + sum(xs.drop(1))
   
   /**
    * This method returns the largest element in a list of integers. If the
